c46d50eb2317427f9d21e76339910ed23dd87112
Chris <1158505+ccomparini@users.noreply.github.com>
2024-06-25 17:14:03 +0000

    Added knuthShuffle
    Added a knuthShuffle method.  Seems to work.
    
    This whole thing could use actual tests (like, statistical ones).

--- randoizer.mjs
+++ randoizer.mjs
@@ -24,20 +24,66 @@
 
 export class Randoizer {
   constructor(seed) {
-    if(typeof seed === "undefined") {
+    if(seed === undefined) {
       seed = Date.now();
     }
     this.reseed(seed);
   }
 
+  /**
+   *  Resets the state of the randomizer to the seed passed.
+   *
+   *  The "seed" can be considered the prior value of next();
+   *  as such, reseeding with a prior next() value resets to
+   *  that point in the sequence.
+   */
   reseed(seed) {
     this.state = BigInt(seed);
   }
 
+  /**
+   *  Returns the next integer in the sequence.
+   */
   next() {
     // https://en.wikipedia.org/wiki/Linear_congruential_generator
     this.state = (1103515245n * this.state + 12345n) % 2147483648n;
     return Number(this.state);
+  }
+
+  /**
+   *  Returns a random number >= min and <= max.
+   *
+   *  Throws an exception on invalid ranges.
+   */
+  nextInRange(min, max) {
+    const rs = max - min + 1;
+    if(rs <= 0) {
+      throw new Error(
+        `invalid range [${min}, ${max}] passed to nextInRange()`
+      );
+    }
+
+    // ... the % here is suspect....
+    // Seems to work though.  shipit.
+    return this.next()%rs + min;
+  }
+
+  /**
+   *  Shuffles the array passed, in place.
+   *
+   *  Modifies the array passed.
+   *
+   *  Returns the array.
+   */
+  knuthShuffle(arr) {
+    for(let el = arr.length - 1; el > 0; el--) {
+      const swapWith = this.nextInRange(0, el);
+
+      const tmp     = arr[el];
+      arr[el]       = arr[swapWith];
+      arr[swapWith] = tmp;
+    }
+    return arr;
   }
 }
 
